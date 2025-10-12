94ba08abe3a3fcefbaf26868d3f0ba8e4a5730d3
Angelo <markclemz86@gmail.com>
2025-10-12 22:15:41 +0800

    Create reversenumber.cpp

---
+++ reversenumber.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+    int num, reverse = 0, rem;
+    
+    cout << "Enter a number sequence: ";
+    cin >> num;
+    
+    while (num != 0) {
+        rem = num % 10;
+        reverse = reverse * 10 + rem;
+        num /= 10;
+    }
+
+    cout << "Reversed Number = " << reverse << endl;
+
+    return 0;
+}
